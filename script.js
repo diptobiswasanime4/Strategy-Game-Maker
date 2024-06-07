9d3d020d8defbf08ca89838c14c5816e290a7646
Dipto Biswas <diptobiswasanime4@gmail.com>
2024-06-07 22:57:34 +0530

    Code

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+class House {
+  constructor({ x, y }) {
+    this.x = x;
+    this.y = y;
+    this.width = 100;
+    this.height = 50;
+  }
+
+  draw(ctx) {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+let houses = [];
+
+addEventListener("click", (e) => {
+  houses.push({ x: e.clientX, y: e.clientY });
+});
+
+houses.forEach((house) => {
+  new House({ x: house.x, y: house.y });
+});
